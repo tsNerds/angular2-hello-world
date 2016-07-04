f274c85c2881a99a893a6b5e99a7447e60f6553d
gion <bogdan.gradinariu@thinslices.com>
2016-07-04 20:37:34 +0300

    fancy template

--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello-world-app',
-  template: 'hello world of angular 2!',
+  template: `
+    <!-- unnecessary comment just to prove that multiline stirngs can easily be achieved -->
+    hello world of {{world}}!
+  `,
 })
 export class HelloWorldAppComponent {
-
+  // the world we live in
+  world:string = 'angular 2';
 }
