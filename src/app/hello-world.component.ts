6329dd00754bfce14982abe7a349e2b3ab6f2da9
gion <bogdan.gradinariu@thinslices.com>
2016-07-04 20:19:02 +0300

    First step 1

---
+++ src/app/hello-world.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world-app',
+  template: 'hello world of angular!',
+})
+export class HelloWorldAppComponent {
+
+}
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { HelloWorldAppComponent } from  './app/hello-world.component';
+
+bootstrap(HelloWorldAppComponent);
+ 
