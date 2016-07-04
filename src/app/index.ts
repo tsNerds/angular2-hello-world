1ee4da378b917c7b073a13b8a15c4391b4976281
gion <bogdan.gradinariu@thinslices.com>
2016-07-04 21:55:25 +0300

    tep 3

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,2 @@
+export const version = 'step-2';
 export {HelloWorldAppComponent} from './hello-world.component';
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { HelloWorldAppComponent } from './app/';
+import { version } from './app/';
+
+console.log('app\'s version is', version);
 
 bootstrap(HelloWorldAppComponent);
